c17718c0b3b0a83fe9a41c258f0234867fc97508
bernd <bernd.cloud@gmail.com>
2015-03-06 20:19:30 +0100

    github test

--- Testing/veme_agt.py
+++ Testing/veme_agt.py
@@ -3,34 +3,42 @@ __author__ = 'bernd'
 
 import argparse
 
-#----------------------
-#   Get CLI Parameter
-#----------------------
+def VeMe_Init():
+    #----------------------
+    #   Get CLI Parameter
+    #----------------------
 
-p = argparse.ArgumentParser(description='Launch VeriMetrix Agent')
-p.add_argument('-c', metavar='<path>',help='Path to config file')
-p.add_argument('-t', metavar='',help='Run agent initialization test only')
-cli_args = p.parse_args()
+    p = argparse.ArgumentParser(description='Launch VeriMetrix Agent')
+    p.add_argument('-c', dest='cfile_path', metavar='<path>',help='Path to config file')
+    p.add_argument('-t', dest='test_run', metavar='',help='Run agent initialization test only')
+    cli_args = p.parse_args()
 
-#----------------------------
-#   Try to read config file
-#----------------------------
+    #----------------------------
+    #   Try to read config file
+    #----------------------------
 
-# if -t print config file
+    # if -t print config file
+    print(cli_args)
 
+    #-------------------------------
+    #   Try to connect ot collector
+    #-------------------------------
 
-#-------------------------------
-#   Try to connect ot collector
-#-------------------------------
+    # try pre-configured domain name or backup IP
+    # check new agent SW version
+    # check for config file backup
 
-# try pre-configured domain name or backup IP
-# check new agent SW version
-# check for config file backup
+    #-----------------------------------
+    #   Try to register with collector
+    #-----------------------------------
 
-#-----------------------------------
-#   Try to register with collector
-#-----------------------------------
+    # provide key and send integrity checksum
 
-# provide key and send integrity checksum
+    # ....
 
-# ....
+def main():
+    VeMe_Init()
+    print('We\'re all just Stardust ...')
+
+if __name__=='__main__':
+    main()
